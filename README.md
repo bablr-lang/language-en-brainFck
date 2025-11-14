0f762f49a62fda6a1405334af405f92a49aad270
Stirling Hostetter <stirlhoss@proton.me>
2025-11-14 15:02:28 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# @bablr/language-en-brainfck
+
+A BABLR language for parsing brainfck
---
+++ test/grammar.test.js
@@ -0,0 +1,30 @@
+import { buildTag } from 'bablr';
+import { spam } from '@bablr/boot';
+import { dedent } from '@qnighy/dedent';
+import language from '../lib/grammar.js';
+import { expect } from 'expect';
+import { printPrettyCSTML } from '@bablr/helpers/tree';
+
+let enhancers = {};
+let { raw } = String;
+
+// enhancers = debugEnhancers;
+
+const buildBrainFckTag = (matcher) => {
+  return buildTag(language, matcher, undefined, { enhancers });
+};
+
+const print = (tree) => {
+  return printPrettyCSTML(tree);
+};
+
+describe('@bablr/language-en-brainFck', () => {
+  describe('Instruction', () => {
+    const brainFck = buildBrainFckTag(spam`<_Instructions />`);
+
+    it('`>`', () => {
+      expect(print(brainFck`>`)).toEqual(dedent`\
+        \n`);
+    });
+  });
+});
